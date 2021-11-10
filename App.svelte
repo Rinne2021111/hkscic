61e448a6751bbfdfc5f5fffaefc716138282e722
Rinne2021111 <92708528+Rinne2021111@users.noreply.github.com>
2021-11-10 17:12:03 +0800

    style A

--- App.svelte
+++ App.svelte
@@ -1,6 +1,14 @@
 <script>
   import Button from "./Button.svelte";
   import Carousel from "./Carousel.svelte";
+  let isShown = false;
+  let images = [
+    "https://static.wixstatic.com/media/038890_da367c978606423c87158638f849ac43~mv2.jpg/v1/fill/w_689,h_459,al_c,q_80,usm_0.66_1.00_0.01/038890_da367c978606423c87158638f849ac43~mv2.webp",
+    "https://static.wixstatic.com/media/038890_0ff05eb9ea254be188cf0a1c4d36bf25~mv2.jpg/v1/fill/w_689,h_459,al_c,q_80,usm_0.66_1.00_0.01/038890_0ff05eb9ea254be188cf0a1c4d36bf25~mv2.webp",
+    "https://static.wixstatic.com/media/038890_07ea1f5cc2b94873b3408831cf195a4c~mv2.jpg/v1/fill/w_689,h_459,al_c,q_80,usm_0.66_1.00_0.01/038890_07ea1f5cc2b94873b3408831cf195a4c~mv2.webp",
+    "https://static.wixstatic.com/media/038890_e6f6851b4d0f4934b17dcf28a29674a8~mv2.jpg/v1/fill/w_689,h_460,al_c,q_80,usm_0.66_1.00_0.01/038890_e6f6851b4d0f4934b17dcf28a29674a8~mv2.webp"
+  ];
+  let hue = 4698;
 </script>
 <style>
   main {
@@ -19,18 +27,17 @@
 	<Button />
   <br>
   <br>
+  {#if isShown}
   <Carousel perPage="1">
+  {#each images as image, i }
   <div class="slide-content">
-     <img alt= "1" src ="https://static.wixstatic.com/media/038890_da367c978606423c87158638f849ac43~mv2.jpg/v1/fill/w_689,h_459,al_c,q_80,usm_0.66_1.00_0.01/038890_da367c978606423c87158638f849ac43~mv2.webp">
+     <img alt= {i} src = {image}/>
   </div>
-  <div class="slide-content">
-     <img alt= "2" src ="https://static.wixstatic.com/media/038890_0ff05eb9ea254be188cf0a1c4d36bf25~mv2.jpg/v1/fill/w_689,h_459,al_c,q_80,usm_0.66_1.00_0.01/038890_0ff05eb9ea254be188cf0a1c4d36bf25~mv2.webp">
-  </div>
-  <div class="slide-content">
-     <img alt= "3" src ="https://static.wixstatic.com/media/038890_07ea1f5cc2b94873b3408831cf195a4c~mv2.jpg/v1/fill/w_689,h_459,al_c,q_80,usm_0.66_1.00_0.01/038890_07ea1f5cc2b94873b3408831cf195a4c~mv2.webp">
-  </div>
-  <div class="slide-content">
-     <img alt= "4" src ="https://static.wixstatic.com/media/038890_e6f6851b4d0f4934b17dcf28a29674a8~mv2.jpg/v1/fill/w_689,h_460,al_c,q_80,usm_0.66_1.00_0.01/038890_e6f6851b4d0f4934b17dcf28a29674a8~mv2.webp">
-  </div>
-</Carousel>
-</main>
+  {/each}
+  </Carousel>
+  {:else}
+   <div
+   on:mousemove={e => hue =e.clientX * e.clientY}
+   style="background: hsl({hue/500},100% ,50%); height:50vh"/>
+  {/if}
+  </main>
